2a1391e918c6db730f9b6f13c45a0bf37a76ea97
Antonina Liybetskaya <tonya1910200479@gmail.com>
2025-04-27 12:58:42 +0300

    Added comment to TaskController

--- src/main/java/com/example/taskmanager/conroller/TaskController.java
+++ src/main/java/com/example/taskmanager/conroller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tasks")
+@RequestMapping("/tasks")// New change for PR
 public class TaskController {
 
     private final TaskService taskService;
